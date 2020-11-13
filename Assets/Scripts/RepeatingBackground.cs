4aadd39357a65604fccb2fea8e3c8f5198577e54
Jim Urbas <git@jimu.net>
2020-11-13 13:41:50 -0600

    Update repeating background

--- Assets/Scripts/RepeatingBackground.cs
+++ Assets/Scripts/RepeatingBackground.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Implements a scrolling background
+// Usage: attach this script to a repeating image.
+// Attach a second copy of the image as a child GameObject to provide seamless repeating texture
+
 #pragma warning disable 0649
 
+[RequireComponent(typeof(MeshCollider))]
 public class RepeatingBackground : MonoBehaviour
 {
+    [Tooltip("Units/second image moves down")]
     [SerializeField] float verticalSpeed;
+
+
 
     public Vector3 translation;
     float offsetY;
